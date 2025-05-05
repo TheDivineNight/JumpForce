a4d971c016f367065e632448fd6f7a4aefcaf403
TheDivineNight <bedoyad@roverkids.org>
2025-05-05 14:15:19 -0400

    Lesson 3.2 Done

--- Unit3/Assets/Scripts/SpawnManager.cs
+++ Unit3/Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,13 @@ public class SpawnManager : MonoBehaviour
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 1, 0);
     private float startDelay = 2;
-    private float repeatRate = 2;   
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
@@ -21,7 +23,11 @@ public class SpawnManager : MonoBehaviour
         
     }void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if(playerControllerScript.gameOver == false)
+        {
+          Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+        
     }
 
 
